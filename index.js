23beaf5bb26b9246f869fff9d81afa81c0edc523
NOAH-KUSH <noahmuthama04@gmail.com>
2025-11-24 21:21:59 +0300

    Update project

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // ====== CONFIG: set this to your deployed Apps Script web app URL ======
-const WEB_APP_URL = "https://script.google.com/macros/s/AKfycbxeuUatvIZLzN6yby4xek3bR7KpxQ_B4UE0VF4Wm1TzIv75GYpv-xrO22kdWpMt_idZ8A/exec"; // <- REPLACE THIS
+const WEB_APP_URL = "https://script.google.com/macros/s/AKfycby3dfLVfubouSv-3y9cTypx2tGcW08qRQciYRTM2PGnJs3FUzhcHmLZaJ9zUaOTdTcnXw/exec"; // <- REPLACE THIS
 
 // ** GLOBAL FLAG: Read the flag set in the HTML files **
 const IS_ADMIN_VIEW = window.IS_ADMIN_VIEW === true;
